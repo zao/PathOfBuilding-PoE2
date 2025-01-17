0550a68756eb5d1d2d78223a8e9fa4e4e46bc41e
LocalIdentity <localidentity2@gmail.com>
2025-01-18 05:34:53 +1100

    Change tree version

--- src/GameVersions.lua
+++ src/GameVersions.lua
@@ -1,13 +1,13 @@
 -- Game versions
 ---Default target version for unknown builds and builds created before 3.0.0.
-legacyTargetVersion = "3_0"
+legacyTargetVersion = "0_0"
 ---Default target for new builds and target to convert legacy builds to.
-liveTargetVersion = "4_0"
+liveTargetVersion = "0_1"
 
 -- Skill tree versions
 ---Added for convenient indexing of skill tree versions.
 ---@type string[]
-treeVersionList = { "4_0" }
+treeVersionList = { "0_1" }
 --- Always points to the latest skill tree version.
 latestTreeVersion = treeVersionList[#treeVersionList]
 ---Tree version where multiple skill trees per build were introduced to PoBC.
@@ -15,8 +15,8 @@ defaultTreeVersion = treeVersionList[1]
 ---Display, comparison and export data for all supported skill tree versions.
 ---@type table<string, {display: string, num: number, url: string}>
 treeVersions = {
-	["4_0"] = {
-		display = "POE 2",
-		num = 4.0,
+	["0_1"] = {
+		display = "0_1",
+		num = 0.1,
 	},
 }
--- src/Modules/Data.lua
+++ src/Modules/Data.lua
@@ -479,29 +479,7 @@ data.setJewelRadiiGlobally = function(treeVersion)
 end
 
 data.jewelRadii = {
-	["3_15"] = {
-		{ inner = 0, outer = 800, col = "^xBB6600", label = "Small" },
-		{ inner = 0, outer = 1200, col = "^x66FFCC", label = "Medium" },
-		{ inner = 0, outer = 1500, col = "^x2222CC", label = "Large" },
-
-		{ inner = 850, outer = 1100, col = "^xD35400", label = "Variable" },
-		{ inner = 1150, outer = 1400, col = "^x66FFCC", label = "Variable" },
-		{ inner = 1450, outer = 1700, col = "^x2222CC", label = "Variable" },
-		{ inner = 1750, outer = 2000, col = "^xC100FF", label = "Variable" },
-		{ inner = 1750, outer = 2000, col = "^xC100FF", label = "Variable" },
-	},
-	["3_16"] = {
-		{ inner = 0, outer = 960, col = "^xBB6600", label = "Small" },
-		{ inner = 0, outer = 1440, col = "^x66FFCC", label = "Medium" },
-		{ inner = 0, outer = 1800, col = "^x2222CC", label = "Large" },
-
-		{ inner = 960, outer = 1320, col = "^xD35400", label = "Variable" },
-		{ inner = 1320, outer = 1680, col = "^x66FFCC", label = "Variable" },
-		{ inner = 1680, outer = 2040, col = "^x2222CC", label = "Variable" },
-		{ inner = 2040, outer = 2400, col = "^xC100FF", label = "Variable" },
-		{ inner = 2400, outer = 2880, col = "^x0B9300", label = "Variable" },
-	},
-	["4_0"] = {
+	["0_1"] = {
 		{ inner = 0, outer = 1000, col = "^xBB6600", label = "Small" },
 		{ inner = 0, outer = 1150, col = "^x66FFCC", label = "Medium" },
 		{ inner = 0, outer = 1300, col = "^x2222CC", label = "Large" },
