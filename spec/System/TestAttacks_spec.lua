e5f35ee3353acc4c11dbf9caab2aed81d22181de
Paliak <91493239+Paliak@users.noreply.github.com>
2025-01-17 17:56:09 +0100

    FIX(tests): inital pass on updating tests for POB2 (#195)

--- spec/System/TestAttacks_spec.lua
+++ spec/System/TestAttacks_spec.lua
@@ -9,27 +9,35 @@ describe("TestAttacks", function()
 
 	it("creates an item and has the correct crit chance", function()
 		assert.are.equals(build.calcsTab.mainOutput.CritChance, 0)
-		build.itemsTab:CreateDisplayItemFromRaw("New Item\nMaraketh Bow\nCrafted: true\nPrefix: None\nPrefix: None\nPrefix: None\nSuffix: None\nSuffix: None\nSuffix: None\nQuality: 20\nSockets: G-G-G-G-G-G\nLevelReq: 71\nImplicits: 1\n{tags:speed}10% increased Movement Speed")
+		build.itemsTab:CreateDisplayItemFromRaw([[
+        	New Item
+        	Heavy Bow
+        ]])
 		build.itemsTab:AddDisplayItem()
 		runCallback("OnFrame")
-		assert.are.equals(build.calcsTab.mainOutput.CritChance, 5.5 * build.calcsTab.mainOutput.HitChance / 100)
+		assert.are.equals(build.calcsTab.mainOutput.CritChance, 5 * build.calcsTab.mainOutput.HitChance / 100)
 	end)
 
 	it("creates an item and has the correct crit multi", function()
-		assert.are.equals(1.5, build.calcsTab.mainOutput.CritMultiplier)
-		build.itemsTab:CreateDisplayItemFromRaw("New Item\nAssassin Bow\nCrafted: true\nPrefix: None\nPrefix: None\nPrefix: None\nSuffix: None\nSuffix: None\nSuffix: None\nQuality: 20\nSockets: G-G-G-G-G-G\nLevelReq: 62\nImplicits: 1\n{tags:damage,critical}{range:0.5}+(15-25)% to Global Critical Strike Multiplier")
+		assert.are.equals(2, build.calcsTab.mainOutput.CritMultiplier)
+		build.itemsTab:CreateDisplayItemFromRaw([[
+        	New Item
+			Heavy Bow
+			25% increased Critical Damage Bonus
+        ]])
 		build.itemsTab:AddDisplayItem()
 		runCallback("OnFrame")
-		assert.are.equals(1.5 + 0.2, build.calcsTab.mainOutput.CritMultiplier)
+		assert.are.equals(2 + 0.25, build.calcsTab.mainOutput.CritMultiplier)
 	end)
 
 	it("correctly converts spell damage per stat to attack damage", function()
 		assert.are.equals(0, build.calcsTab.mainEnv.player.modDB:Sum("INC", { flags = ModFlag.Attack }, "Damage"))
 		build.itemsTab:CreateDisplayItemFromRaw([[
 		New Item
-		Coral Amulet
+		Ring
 		10% increased attack damage
 		10% increased spell damage
+		+20 to Intelligence
 		1% increased spell damage per 10 intelligence
 		]])
 		build.itemsTab:AddDisplayItem()
@@ -40,12 +48,11 @@ describe("TestAttacks", function()
 
 		build.itemsTab:CreateDisplayItemFromRaw([[
 		New Item
-		Coral Ring
+		Ring
 		increases and reductions to spell damage also apply to attacks
 		]])
 		build.itemsTab:AddDisplayItem()
 		runCallback("OnFrame")
 		assert.are.equals(22, build.calcsTab.mainEnv.player.mainSkill.skillModList:Sum("INC", { flags = ModFlag.Attack }, "Damage"))
-
 	end)
 end)
