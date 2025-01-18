d2b768c2e505e403333ff55513d3323afb45e800
Tomas Slusny <slusnucky@gmail.com>
2025-01-18 22:57:59 +0100

    Fix parsing for Grand Spectrum (#46)
    pob: https://pobb.in/v3qpSIuYSi6A
    Closes https://github.com/PathOfBuildingCommunity/PathOfBuilding-PoE2/issues/270
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/Modules/ModParser.lua
+++ src/Modules/ModParser.lua
@@ -1356,7 +1356,7 @@ local modTagList = {
 	["per allocated mastery passive skill"] = { tag = { type = "Multiplier", var = "AllocatedMastery" } },
 	["per allocated notable passive skill"] = { tag = { type = "Multiplier", var = "AllocatedNotable" } },
 	["for each different type of mastery you have allocated"] = { tag = { type = "Multiplier", var = "AllocatedMasteryType" } },
-	["per grand spectrum"] = { tag = { type = "Multiplier", var = "GrandSpectrum" } },
+	["per socketed grand spectrum"] = { tag = { type = "Multiplier", var = "GrandSpectrum" } },
 	["per second you've been stationary, up to a maximum of (%d+)%%"] = function(num) return { tag = { type = "Multiplier", var = "StationarySeconds", limit = tonumber(num), limitTotal = true } } end,
 	["per elemental ailment you've inflicted recently"] = { tag = { type = "Multiplier", var = "AppliedAilmentsRecently" } },
 	["per tailwind"] = { tag = { type = "Multiplier", var = "Tailwind" } },
