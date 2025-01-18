02a6a65031b8118a0553feff1c6dbf70eaad2198
LocalIdentity <31035929+LocalIdentity@users.noreply.github.com>
2025-01-19 10:03:27 +1100

    Fix Arrow Speed not applying to Feathered Fletchingnode (#50)
    Arrow speed uses ModFlag.Bow which was being ignored before
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>

--- src/Modules/CalcOffence.lua
+++ src/Modules/CalcOffence.lua
@@ -622,7 +622,7 @@ function calcs.offence(env, actor, activeSkill)
 	end
 	if skillModList:Flag(nil, "ProjectileSpeedAppliesToBowDamage") then
 		-- Bow mastery projectile speed to damage with bows conversion
-		for i, value in ipairs(skillModList:Tabulate("INC", { }, "ProjectileSpeed")) do
+		for i, value in ipairs(skillModList:Tabulate("INC", { flags = ModFlag.Bow }, "ProjectileSpeed")) do
 			local mod = value.mod
 			skillModList:NewMod("Damage", mod.type, mod.value, mod.source, bor(ModFlag.Bow, ModFlag.Hit), mod.keywordFlags, unpack(mod))
 		end
